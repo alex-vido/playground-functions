65f0b34b79838bc695517fa004f8fd51794bdda1
Alex Vido <alexvidodev@gmail.com>
2023-04-06 12:01:44 -0300

    Feito exercício 11, quase morri pra verificar se tinha 3 ou 4 repetidos kkk

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,29 @@
 // Desafio 11 - Crie a função generatePhoneNumber
-
+const generatePhoneNumber = (arr) => {
+  let counter = {};
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] < 0 || arr[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    if ((typeof counter[arr[i]] === 'undefined')) {
+      counter[arr[i]] = 1;
+    } else {
+      counter[arr[i]] += 1;
+    }
+    if (counter[arr[i]] >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+  }
+  }
+  let telefoneArray = arr;
+  telefoneArray.splice(0, 0,'(');
+  telefoneArray.splice(3, 0,')');
+  telefoneArray.splice(4, 0,' ');
+  telefoneArray.splice(10, 0,'-');
+  return telefoneArray.join('');
+ };
 // Desafio 12 -  Crie a função triangleCheck
 
 // Desafio 13 - Crie a função hydrate
